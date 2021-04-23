41340d583c6d000134affc26b692fec33a57e8d1
Rikesh Subedi <googrikesh@gmail.com>
2021-04-23 08:52:53 +0530

    Update spreadsheet_config.js

--- routes/spreadsheet_config.js
+++ routes/spreadsheet_config.js
@@ -1,12 +1,12 @@
 exports.sConfig = {
     "debug": true,
-    "spreadsheetId": '12Fc2PeBYeOeQb5QtbpRVz4lOekIVPru0E8VF5hgjK60',
-    "worksheetName": 'first',
+    "spreadsheetId": '<your spreadsheet id>',
+    "worksheetName": '<name of the worksheet>',
     "worksheetId": "od6",
 
     oauth: {
-        email: '369778194884-chls77otv3u2nbinrnlesqho6ssk4jem@developer.gserviceaccount.com',
-        keyFile: 'C:/Users/i077592/Documents/google_keys/my_google_pem.pem'
+        email: '<your oauth email>',
+        keyFile: '<path to your my_google_pem.pem>'
     }
 
 }
